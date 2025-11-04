5ee31b7f99ffe62a30d7355159a9c64c184ecb12
PeterMartEsc <petermartesc@gmail.com>
2025-11-04 16:04:43 +0000

    feat(shared, main) created echos urls and base/index templates

---
+++ tribu/echos/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path #, register_converter
+from . import views #, converters
+
+app_name = 'echos'
+# register_converter(converters.PostConverter, 'echo')
+
+
+urlpatterns = [
+    # path('', views.post_list, name='post-list'),
+    # path('add/', views.add_post, name='add-post'),
+    # path('<post:post>/delete/', views.delete_post, name='delete-post'),
+    # path('<post:post>/', views.post_detail, name='post-detail'),
+    # path('<post:post>/edit/', views.edit_post, name='edit-post'),
+]
--- tribu/main/urls.py
+++ tribu/main/urls.py
@@ -15,8 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.shortcuts import render
 
 urlpatterns = [
+    path('', lambda r: render(r, 'index.html'), name='index'),
     path('admin/', admin.site.urls),
+    # path('echos/', include('echos.urls'))
 ]
